f2264d62832be4d40ae4d6375a8b3d806d97d86d
duwianjar06-ctrl <duwianjar06@gmail.com>
2026-08-27 11:39:18 +0700

    test: fix laravel test bootstrap and api prefixes

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AutomationController;
 
-Route::get('/api/health', [AutomationController::class, 'health']);
-Route::post('/api/cron/news-publish', [AutomationController::class, 'newsPublish']);
-Route::post('/api/cron/social-prepare', [AutomationController::class, 'socialPrepare']);
-Route::post('/api/cron/social-publish', [AutomationController::class, 'socialPublish']);
-Route::get('/api/admin/news', [AutomationController::class, 'newsData']);
-Route::get('/api/admin/instagram', [AutomationController::class, 'instagramData']);
-Route::get('/api/admin/health', [AutomationController::class, 'health']);
+Route::get('/health', [AutomationController::class, 'health']);
+Route::post('/cron/news-publish', [AutomationController::class, 'newsPublish']);
+Route::post('/cron/social-prepare', [AutomationController::class, 'socialPrepare']);
+Route::post('/cron/social-publish', [AutomationController::class, 'socialPublish']);
+Route::get('/admin/news', [AutomationController::class, 'newsData']);
+Route::get('/admin/instagram', [AutomationController::class, 'instagramData']);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,4 @@
 <?php
 namespace Tests;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Foundation\Testing\Concerns\CreatesApplication;
 abstract class TestCase extends BaseTestCase { use CreatesApplication; }
