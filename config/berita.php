6e442461836e94ffd1699237e31c34217e53cd58
duwianjar06-ctrl <duwianjar06@gmail.com>
2026-09-08 04:34:36 +0700

    fix: repair news config

--- config/berita.php
+++ config/berita.php
@@ -1,7 +1,2 @@
 <?php
-return [
-    'automation_enabled' => env('AUTOMATION_ENABLED', false),
-    'instagram_preparation_enabled' => env('INSTAGRAM_PREPARATION_ENABLED', false),
-    'instagram_auto_publish' => env('INSTAGRAM_AUTO_PUBLISH', false),
-    'instagram_auto_publish' => env('INSTAGRAM_AUTO_PUBLISH', false),
-];
+return ['automation_enabled'=>env('AUTOMATION_ENABLED',false)];
