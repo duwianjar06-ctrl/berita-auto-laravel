41ebd99935ca6d70571eacdbbe1d5da67d516729
duwianjar06-ctrl <duwianjar06@gmail.com>
2026-09-08 22:34:00 +0700

    feat: add news quality gate and generation safety

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ Route::get('/health', [AutomationController::class, 'health']);
 Route::post('/cron/news-publish', [AutomationController::class, 'newsPublish']);
 Route::post('/cron/social-prepare', [AutomationController::class, 'socialPrepare']);
 Route::post('/cron/social-publish', [AutomationController::class, 'socialPublish']);
-Route::get('/admin/news', [AutomationController::class, 'newsData']);
+Route::middleware(['web', \App\Http\Middleware\EnsureAdmin::class])->group(function () { Route::get('/admin/news', [AutomationController::class, 'newsData']); });
 Route::get('/admin/instagram', [AutomationController::class, 'instagramData']);
